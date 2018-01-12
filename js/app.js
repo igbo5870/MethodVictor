cd742e98d25f50998bb884b474a0cd3aa2310795
Mathéo <matheo.zeller@gmail.com>
2018-01-12 16:44:55 +0100

    commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 (function main(){
 	$('.check div img').mouseenter(function(){
-		$(".check div img").css({"transform":"rotate(360deg)","transition":"all 0.8s ease-in-out 0s"})
-		console.log("cou")
+		$(".check div img").css({"transform":"rotate(180deg)","transition":"all 0.8s ease-in-out 0s"})
 	})
 	$('.check div img').mouseleave(function(){
-		$(".check div img").css({"transform":"rotate(-360deg)","transition":"all 0.8s ease-in-out 0s"})
-		console.log("cou")
+		$(".check div img").css({"transform":"rotate(-180deg)","transition":"all 0.8s ease-in-out 0s"})
 	})
 })();
